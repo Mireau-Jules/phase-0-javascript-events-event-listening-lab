7266e7ee1d48a8b16a997e8b74ffd596573ac8be
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-05-10 09:48:27 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function addingEventListener() {
     const input = document.getElementById('button')
     input.addEventListener('click', function (){alert('I was clicked! ')})
 }
+
+addingEventListener()
